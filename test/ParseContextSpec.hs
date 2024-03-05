b1ef23191e1827bb523477c706a3e028889a0020
CrystalSplitter <gamewhizzit@gmail.com>
2024-03-04 22:03:41 -0800

    Reorganise ParseContextSpec fixtures

--- test/ParseContextSpec.hs
+++ test/ParseContextSpec.hs
@@ -11,14 +11,18 @@ import qualified Ghcitui.Loc as Loc
 spec :: Spec
 spec = do
     describe "parseContext" $ do
+        it "can parse fibonacci context" $ do
+            let expectedLoc =
+                    Loc.SourceRange
+                        { Loc.startLine = Just 9
+                        , Loc.startCol = Just 17
+                        , Loc.endLine = Just 9
+                        , Loc.endCol = Just 29
+                        }
+            let expected =
+                    PC.PCContext (PC.ParseContextOut "Yib.fibty.right" "test/Fib.hs" expectedLoc)
+            PC.parseContext fibFixture `shouldBe` expected
         it "can parse the Ormolu function parseModule' (with an apostraphe)" $ do
-            let apostrapheFixture =
-                    T.unlines
-                        [ "()"
-                        , "[src/Ormolu.hs:(257,51)-(261,35)] #~GHCID-START~#()"
-                        , "[src/Ormolu.hs:(257,51)-(261,35)] #~GHCID-START~#--> invoke"
-                        , "  Stopped in Ormolu.parseModule', src/Ormolu.hs:(257,51)-(261,35)"
-                        ]
             let expectedLoc =
                     Loc.SourceRange
                         { Loc.startLine = Just 257
@@ -30,3 +34,20 @@ spec = do
                     PC.PCContext
                         (PC.ParseContextOut "Ormolu.parseModule'" "src/Ormolu.hs" expectedLoc)
             PC.parseContext apostrapheFixture `shouldBe` expected
+
+
+fibFixture :: T.Text
+fibFixture =
+    T.unlines
+        [ "[test/Fib.hs:9:17-29] #~GHCID-START~#[test/Fib.hs:9:17-29] #~GHCID-START~#--> fibty 10"
+        , "  Stopped in Yib.fibty.right, test/Fib.hs:9:17-29"
+        ]
+
+apostrapheFixture :: T.Text
+apostrapheFixture =
+    T.unlines
+        [ "()"
+        , "[src/Ormolu.hs:(257,51)-(261,35)] #~GHCID-START~#()"
+        , "[src/Ormolu.hs:(257,51)-(261,35)] #~GHCID-START~#--> invoke"
+        , "  Stopped in Ormolu.parseModule', src/Ormolu.hs:(257,51)-(261,35)"
+        ]
